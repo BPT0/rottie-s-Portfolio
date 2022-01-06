a677453fe38c20be2d8616910bb2483895058411
PTFDB <kbs95123@gmail.com>
2022-01-06 19:52:11 +0900

    velog 포트폴리오 생성

---
+++ velog/app/src/main/java/com/example/velog/main.java
@@ -0,0 +1,15 @@
+package com.example.velog;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class main extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
